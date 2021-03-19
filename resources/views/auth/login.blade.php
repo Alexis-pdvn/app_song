eac834c945afefda9f01a9bdfbf9f52412a63b5d
Edouard Bucamp <edouard.bucamp@gmail.com>
2021-03-20 00:56:37 +0100

    update page login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,25 @@
 @extends('templates.general')
 
 @section('contenu')
+        <div class="loginMain">
             <div class="loginLeft">
 
             </div>
 
-            <div>
-                <div>{{ __('Login') }}</div>
+            <div class="loginRight">
+                <div id="containerLogin">
+                    <div class="loginRight__title">
+                        <h1 class="loginRight__title-title">Login</h1>
+                        <div class="loginRight__title-special"></div>
+                    </div>
 
-                <div>
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                    <div>
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
 
-                        <!-- Div pour la partie email -->
-                        <div>
-                            <label for="email">{{ __('E-Mail Address') }}</label>
-
+                            <!-- Div pour la partie email -->
                             <div>
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" Placeholder="E-mail" type="email" class="loginRight__form-input form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -25,15 +27,11 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
 
-
-                        <!-- Div pour la partie message -->
-                        <div>
-                            <label for="password" >{{ __('Password') }}</label>
-
+                            <!-- Div pour la partie mot de passe -->
+                            
                             <div>
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" Placeholder="Password" type="password" class="loginRight__form-input form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -41,37 +39,32 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+                        
+                            <!-- Div pour la partie Remember -->
+                            <div class="loginRight__remember">
+                                <input class="loginRight__remember-checkbox" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                        <!-- Div pour la partie Remember -->
-                        <div>
-                            <div>
-                                <div>
-                                    <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                                <label class="loginRight__remember-text" for="remember">
+                                    {{ __('Remember Me') }}
+                                </label>
                             </div>
-                        </div>
 
-                        <!-- Div pour le bouton et oublie mdp -->
-                        <div>
+                            <!-- Div pour le bouton et oublie mdp -->
                             <div>
-                                <button type="submit" class="loginBtn">
-                                    {{ __('Login') }}
+                                <button type="submit" class="loginRight__button">
+                                    {{ __('Connection') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
                                     <a href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('Forgot Your Password ?') }}
                                     </a>
                                 @endif
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
+        </div>
 
 @endsection
--- resources/views/templates/general.blade.php
+++ resources/views/templates/general.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Riddle - Portfolio Template</title>
+    <title>MMI'SOUND</title>
     <meta charset="UTF-8">
     <meta name="description" content="Pictures, laravel">
     <meta name="keywords" content="Pictures, laravel">
