57098bfbb24c528ed04bb0d1f437700292834996
Edouard Bucamp <edouard.bucamp@gmail.com>
2021-03-24 23:38:17 +0100

    stylisation page user

--- resources/views/firstcontroller/utilisateur.blade.php
+++ resources/views/firstcontroller/utilisateur.blade.php
@@ -2,69 +2,51 @@
 
 @section('contenu')
 
-<section class="intro-section">
-    <div class="container text-center">
-        <div class="row">
-            <div class="col-xl-10 offset-xl-1">
-                <h2 class="section-title">Vous êtes sur la page de <span>{{$user->name}}</span></h2>
+    <section class="container userMain">
+        <h2>You are on the <span>{{$user->name}}</span>'s profile</h2>
+
+        <div class="userProfile">
+            <div class="userProfile__img">
+                <img src="{{ $user->avatar }}" alt="">
             </div>
-        </div>
-    </div>
-</section>
+            <div class="userProfile__info">
+                <p class="userProfile__info--descTitle">Description :</p>
+                <p class="userProfile__info--descText">{{ $user->overview }}</p>
+                    <div class="userProfile__info--fieldMain">
+                        <div>
+                            <div>
+                                <h2 class="userProfile__info--field">{{$user->theyLikeMe()->count()}}<span>&nbsp; Subscription</span></h2>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <h2 class="userProfile__info--field">{{$user->ILikeThem()->count()}}<span>&nbsp; Subscriber</span></h2>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <h2 class="userProfile__info--field">{{$user->songs()->count()}}<span>&nbsp; Songs</span></h2>
+                            </div>
+                        </div>
+                    </div>
 
-<section class="page-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6">
-                <p>{{ $user->overview }}</p>
-                <div class="row mt-5">
-                    <div class="col-md-4">
-                        <div class="milestone">
-                            <h2>{{$user->theyLikeMe()->count()}}<span>Abonnements</span></h2>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="milestone">
-                            <h2>{{$user->ILikeThem()->count()}}<span>Abonnés</span></h2>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="milestone">
-                            <h2>{{$user->songs()->count()}}<span>Musique(s)</span></h2>
-                        </div>
-                    </div>
-                </div>
-
-                @auth
-                
-                    @if(Auth::id() !=$user->id)
-                        @if(Auth::user()->ILikeThem->contains($user->id))
-                            <a href="/suivre/{{ $user->id }}" class="site-btn btn-line">Suivi</a>
-                        @else
-                            <a href="/suivre/{{ $user->id }}" class="site-btn btn-fade ">Suivre</a>
+                    @auth
+                        @if(Auth::id() !=$user->id)
+                            @if(Auth::user()->ILikeThem->contains($user->id))
+                                <a href="/suivre/{{ $user->id }}">Suivi</a>
+                            @else
+                                <a href="/suivre/{{ $user->id }}">Suivre</a>
+                            @endif
                         @endif
-
-                    @endif
-
-                @endauth
-
-            </div>
-            <div class="col-lg-5 offset-lg-1">
-                <figure class="pic-frame">
-                    <img src="{{ $user->avatar }}" alt="">
-                </figure>
+                    @endauth
             </div>
         </div>
-        
-    </div>
-</section>
+    </section>
 
-<div class="container-fluid p-0">
-                <h2>Retrouvé mes chansons</h2>
-    <div class="playlist container">
-        @include("partials._songs", ["songs" => $user->songs])
-
+    <div>
+        <div class="playlist container">
+            @include("partials._songs", ["songs" => $user->songs])
+        </div>
     </div>
-</div>
 
 @endsection
